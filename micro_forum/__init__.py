aa91a1dd1e1b6e2e12ca086d2768facde1fa3659
wegfawefgawefg <668es218pur@gmail.com>
2023-08-02 23:10:27 -0500

    no u

--- micro_forum/__init__.py
+++ micro_forum/__init__.py
@@ -1,7 +1,14 @@
+import os
+import json
 from flask import Flask
+
 db = "messages.json"
 
+if not os.path.isfile(db):
+    with open("./default_messages.json") as f:
+        j = json.load(f)
+    with open(db, "w") as f:
+        json.dump(j, f)
 
 app = Flask(__name__)
 from .host import *
-
--- micro_forum/host.py
+++ micro_forum/host.py
@@ -8,6 +8,7 @@ from flask import url_for, redirect
 
 from micro_forum import app, db
 
+
 def get_last_n(N=100):
     with open(db) as f:
         j = json.load(f)
@@ -15,26 +16,26 @@ def get_last_n(N=100):
         last_n = list(reversed(messages[-N:]))
     return last_n
 
+
 def add_message(message):
     with open(db) as f:
         j = json.load(f)
         messages = j["messages"]
-        new_message = {
-            "time": datetime.datetime.now().strftime("%c"),
-            "msg": message}
+        new_message = {"time": datetime.datetime.now().strftime("%c"), "msg": message}
         messages.append(new_message)
-        
-    with open(db, 'w') as f:
+
+    with open(db, "w") as f:
         json.dump(j, f)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def main():
-    if request.method == 'GET':
-        new_message = request.args.get('nm')
+    if request.method == "GET":
+        new_message = request.args.get("nm")
         if new_message:
             if len(new_message) < 10000:
                 add_message(new_message)
-                return redirect(url_for('main'))
+                return redirect(url_for("main"))
 
     last_n_message_items = get_last_n()
     return render_template("minimal_forum.html", message_items=last_n_message_items)
